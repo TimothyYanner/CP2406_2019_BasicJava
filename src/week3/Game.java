981d840794237ef53ab5c003bb91a10d7a227d9f
TimothyYanner <48793695+TimothyYanner@users.noreply.github.com>
2019-08-21 21:09:00 -0700

    Fix secret error

--- src/week3/Game.java
+++ src/week3/Game.java
@@ -5,20 +5,19 @@ import java.util.Scanner;
 
 public class Game {
     private Random random = new Random();
-    int secret = random.nextInt(10) + 1;
+    int secret;
     int count;
     int wins;
 
     public static void main(String[] args) {
-        Game game = new Game();
+        Game game = new Game(5, 3);
         Scanner inputDevice = new Scanner(System.in);
         System.out.print("Guess the number >>> ");
         int numberGuess = inputDevice.nextInt();
         game.makeGuess(numberGuess);
         if (game.wins == 1) {
             System.out.println("You Won!");
-        }
-        else {
+        } else {
             System.out.println("You Lost");
         }
     }
@@ -27,9 +26,12 @@ public class Game {
         if (value <= 10 && value > 0) {
             this.count += 1;
             this.wins += 1;
-        }
-        else {
+        } else {
             this.count += 1;
         }
+    }
+
+    public Game(int min, int max) {
+        this.secret = random.nextInt((max - min) + 1 + min);
     }
 }
--- src/week3/GameTest.java
+++ src/week3/GameTest.java
@@ -7,13 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class GameTest {
     @Test
     void checkSecret() {
-        Game game = new Game();
+        Game game = new Game(3, 5);
         boolean validSecret = game.secret > 0 && game.secret < 11;
         assertTrue(validSecret);
     }
 
     @Test void checkPlayCount() {
-        Game game = new Game();
+        Game game = new Game(3, 5);
         assertEquals(0, game.count);
         assertEquals(0, game.wins);
 
