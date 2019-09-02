e17f9fc92109d9275e9f9bb4bffc769a5898211d
TimothyYanner <48793695+TimothyYanner@users.noreply.github.com>
2019-09-02 22:09:02 +1000

    Write CarCareChoice and CarCareChoice2

---
+++ src/week6/CarCareChoice.java
@@ -0,0 +1,42 @@
+package week6;
+
+import java.util.Scanner;
+
+public class CarCareChoice {
+    public static void main(String[] args) {
+        Scanner inputDevice = new Scanner(System.in);
+        String[] services = {"oil change", "tire rotation", "battery check", "brake inspection"};
+        int[] prices = {25, 22, 15, 5};
+        System.out.println("Choose from the following services: oil change | tire rotation | battery check | brake inspection");
+        String userInput = inputDevice.nextLine();
+        while(!checkInput(userInput, services)) {
+            System.out.println("That is not a valid option");
+            System.out.println("Choose from the following services: oil change | tire rotation | battery check | brake inspection");
+            userInput = inputDevice.nextLine();
+        }
+        int index = checkIndex(userInput, services);
+        System.out.print("You have selected " + services[index] + " for $" + prices[index]);
+    }
+
+    public static boolean checkInput(String input, String[] array) {
+        int i;
+        boolean check = false;
+        for (i = 0; i < array.length; ++i) {
+            if (input.equals(array[i])) {
+                check = true;
+            }
+        }
+        return check;
+    }
+
+    public static int checkIndex(String input, String[] array) {
+        int index = 0;
+        int i;
+        for (i = 0; i < array.length; ++i) {
+            if (input.equals(array[i])) {
+                index = i;
+            }
+        }
+        return index;
+    }
+}
