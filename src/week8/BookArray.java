26a9b9f660be56bd8def3a327315c55ede6a34e2
TimothyYanner <48793695+TimothyYanner@users.noreply.github.com>
2019-09-17 00:57:32 +1000

    Write BookArray class

---
+++ src/week8/BookArray.java
@@ -0,0 +1,20 @@
+package week8;
+
+public class BookArray {
+    public static void main(String[] args) {
+        Book[] bookArray = new Book[10];
+        int i;
+        for (i = 0; i < bookArray.length; ++i) { // generate fiction and non fiction books because im too lazy to manually write 10 books out
+            Book newBook;
+            if (i % 2 == 0) {
+                newBook = new Fiction("fiction book");
+            } else {
+                newBook = new NonFiction("non-fiction book");
+            }
+            bookArray[i] = newBook;
+        }
+        for (i = 0; i < bookArray.length; ++i) {
+            System.out.println("The book " + bookArray[i].title + " costs $" + bookArray[i].price); // print details about all books
+        }
+    }
+}
