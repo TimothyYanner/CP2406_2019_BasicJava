c2c89858dea5c70a8fae014b466cab9d2fd1da75
TimothyYanner <48793695+TimothyYanner@users.noreply.github.com>
2019-09-17 14:18:17 +1000

    Write BookArray class

--- src/week8/BookArray.java
+++ src/week8/BookArray.java
@@ -4,7 +4,7 @@ public class BookArray {
     public static void main(String[] args) {
         Book[] bookArray = new Book[10];
         int i;
-        for (i = 0; i < bookArray.length; ++i) { // generate fiction and non fiction books because im too lazy to manually write 10 books out
+        for (i = 0; i < bookArray.length; ++i) { // generate fiction and non fiction books
             Book newBook;
             if (i % 2 == 0) {
                 newBook = new Fiction("fiction book");
