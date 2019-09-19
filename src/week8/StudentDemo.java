24d0110cb0b1f30f6cf314043ca09c1c8354d98d
TimothyYanner <48793695+TimothyYanner@users.noreply.github.com>
2019-09-19 13:38:36 +1000

    Write Student, UndergraduateStudent, GraduateStudent, StudentAtLarge, and StudentDemo classes

--- src/week8/StudentDemo.java
+++ src/week8/StudentDemo.java
@@ -18,8 +18,26 @@ public class StudentDemo {
         graduateStudent1.setAnnualTuition();
 
         GraduateStudent graduateStudent2 = new GraduateStudent();
-        graduateStudent2.setIdNumber(789);
-        graduateStudent2.setLastName("James");
+        graduateStudent2.setIdNumber(321);
+        graduateStudent2.setLastName("Thomas");
         graduateStudent2.setAnnualTuition();
+
+        StudentAtLarge studentAtLarge1 = new StudentAtLarge();
+        studentAtLarge1.setIdNumber(987);
+        studentAtLarge1.setLastName("John");
+        studentAtLarge1.setAnnualTuition();
+
+        StudentAtLarge studentAtLarge2 = new StudentAtLarge();
+        studentAtLarge2.setIdNumber(654);
+        studentAtLarge2.setLastName("Jane");
+        studentAtLarge2.setAnnualTuition();
+
+        Student[] students = {undergraduateStudent1, graduateStudent1, studentAtLarge1, undergraduateStudent2, graduateStudent2, studentAtLarge2};
+
+        int i;
+        for (i = 0; i < students.length; ++i) {
+            System.out.println(students[i].getLastName() + " has the ID number " + students[i].getIdNumber() +
+                    " and an annual tuition of $" + students[i].getAnnualTuition());
+        }
     }
 }
