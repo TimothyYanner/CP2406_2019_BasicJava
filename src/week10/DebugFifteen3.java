1d746ca4d602e7600ea0d8441ff048888bcc7b7b
TimothyYanner <48793695+TimothyYanner@users.noreply.github.com>
2019-10-08 16:43:55 +1000

    Fix Debug 15s

---
+++ src/week10/DebugFifteen3.java
@@ -0,0 +1,50 @@
+package week10;
+// Lets user type keys
+// Displays each key typed
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+public class DebugFifteen3 extends JFrame implements KeyListener
+{
+   char key;
+   Container con = null;
+   GridLayout grid = new GridLayout(2, 1);
+   JLabel label = new JLabel("Key Typed:");
+   JTextArea textArea = new JTextArea(4, 25);
+   public DebugFifteen3()
+   {
+      setTitle("Debug Key Frame");
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setLayout(grid);
+      add(textArea);
+      add(label);
+      textArea.addKeyListener(this);
+   }
+
+   @Override
+   public void keyTyped(KeyEvent e)
+   {
+      key = e.getKeyChar();
+      label.setText ("Key Typed: " + key);
+   }
+
+   @Override
+   public void keyPressed(KeyEvent e) {
+
+   }
+
+   @Override
+   public void keyReleased(KeyEvent e) {
+
+   }
+
+
+   public static void main(String[] args)
+   {
+      DebugFifteen3 kFrame = new DebugFifteen3();
+      kFrame.setSize(300, 200);
+      kFrame.setVisible(true);
+   }
+}
+
+
